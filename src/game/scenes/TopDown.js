1041c1791b6f9ec70beceabc437c660e178908d2
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-02-07 22:39:07 -0500

    3

--- src/game/scenes/TopDown.js
+++ src/game/scenes/TopDown.js
@@ -29,3 +29,14 @@ export class TopDown extends Phaser.Scene
 }
 
 //            console.dir(e); // This shows all properties (and methods) inside an object. Replace E with object.
+
+/*
+create() {
+    // This creates an object with keys mapped to specific codes
+    this.keys = this.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+    });
+}*/
