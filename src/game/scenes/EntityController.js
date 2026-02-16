9190535353741bfdc4fec96be65136a73b84eea7
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-02-16 09:46:39 -0500

    1

--- src/game/scenes/EntityController.js
+++ src/game/scenes/EntityController.js
@@ -2,31 +2,31 @@ export default class EntityController {
     constructor(entity, speed = 160) {
         this.entity = entity;
         this.speed  = speed
-        this.velocityX = 0;
-        this.velocityY = 0;
+        this.directionX = 0;
+        this.directionY = 0;
     }
 
     update(){
-        this.applyVelocity();
+        this.entity.setVelocity(
+            this.directionX * this.speed, 
+            this.directionY * this.speed
+        );
+
+        if (this.directionX !== 0 && this.directionY !== 0) {
+            this.entity.body.velocity.normalize().scale(this.speed);
+        }
+    
     }
 
     setDirection(x, y) {
-        this.velocityX = x * this.speed;
-        this.velocityY = y * this.speed;
+        this.directionX = x;
+        this.directionY = y;
     }
 
     stop(){
-        this.velocityX = 0;
-        this.velocityY = 0;
+        this.directionX = 0;
+        this.directionY = 0;
     }
 
-    applyVelocity () {
-        this.entity.setVelocity(this.velocityX, this.velocityY);
-
-        if (this.velocityX !== 0 && this.velocityY !== 0) {
-            this.entity.body.velocity.normalize().scale(this.speed);
-        }
-    }
-
-
+    
 }
