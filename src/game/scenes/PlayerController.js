bea4e5ffdaf8c0a759b54a8ee41bc9d90469fa8f
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-02-15 22:22:49 -0500

    argg

--- src/game/scenes/PlayerController.js
+++ src/game/scenes/PlayerController.js
@@ -13,21 +13,21 @@ export default class PlayerController {
         let y = 0;
 
         if (this.cursors.left.isDown || this.wasd.left.isDown) {
-            x = -1;
+            x -= 1;
             this.facing.set(-1, 0);
         }
         if (this.cursors.right.isDown || this.wasd.right.isDown) {
-            x = 1;
+            x += 1;
             this.facing.set(1, 0);
         }
 
         if (this.cursors.up.isDown || this.wasd.up.isDown) {
-            y = -1;
+            y -= 1;
             this.facing.set(0,-1);
         }
 
         if (this.cursors.down.isDown || this.wasd.down.isDown) {
-            y = 1;
+            y += 1;
             this.facing.set(0, 1)
         }
 
@@ -38,3 +38,16 @@ export default class PlayerController {
         }
     }
 }
+
+/*3. A Small "Facing" Detail
+In your PlayerController, make sure you still wrap the this.facing.set(x, y) call in a check:
+javascript
+if (x !== 0 || y !== 0) {
+    this.facing.set(x, y).normalize();
+    this.movement.setDirection(x, y);
+} else {
+    this.movement.stop();
+}
+Use code with caution.
+
+If you don't check for x !== 0 || y !== 0 before setting the vector, and the player stops, this.facing would be set to (0, 0). By wrapping it, you preserve that last moved direction you wanted*/
